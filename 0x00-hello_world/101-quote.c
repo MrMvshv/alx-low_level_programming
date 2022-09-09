f09dd1dc5a7690f2afd73ab44f6f3cba99a66ced
MrMvshv <machariajosphat001@gmail.com>
2022-09-09 03:35:58 +0000

    software 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints out a string with special characters
+ *
+ * Description: tests the use of escape characters.
+ *Return: always returns 1.
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(str, 1, sizeof(str), stderr);
+	return (1);
+}
+
