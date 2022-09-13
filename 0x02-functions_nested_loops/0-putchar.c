3ba4b51cb960a64ea08a184927ea217e8ced6a59
MrMvshv <machariajosphat001@gmail.com>
2022-09-13 12:08:50 +0000

    Darling Betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
-/* include "_putchar.c" */
 #include "main.h"
-
+/**
+ * main - prints _putchar followed by newline
+ *
+ * Return: 0 when success
+ */
 int main(void)
 {
 	putch();
 	_putchar(10);
 	return (0);
 }
+/**
+ * putch - loops pt[] to print string
+ *
+ * uses _putchar function
+ * Return: 0 when succes
+ */
 
 int putch(void)
 {
 	int i;
 	char pt[] = "_putchar";
+
 	for (i = 0; i < 8; i++)
 	{
 		_putchar(pt[i]);
