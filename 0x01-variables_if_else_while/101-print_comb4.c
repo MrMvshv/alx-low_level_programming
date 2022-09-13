df3b93149605290169e33078d6eb523142de6eb1
MrMvshv <machariajosphat001@gmail.com>
2022-09-13 03:05:58 +0000

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,36 +8,36 @@
 
 int main(void)
 {
-		int i = 48;
-		int e = 48;
-		int a = 48;
+	int i = 48;
+	int e = 48;
+	int a = 48;
 
-		while (a < 58)
+	while (a < 58)
+	{
+		e = 48;
+		while (e < 58)
 		{
-			e = 48;
-			while (e < 58)
+			i = 48;
+			while (i < 58)
 			{
-				i = 48;
-				while(i < 58)
+				if (e != i && a != e && a < e && e < i)
 				{
-					if (e != i && a != e && a < e && e < i)
+					putchar(a);
+					putchar(e);
+					putchar(i);
+					if (i == 57 && e == 56 && a == 55)
 					{
-						putchar(a);
-						putchar(e);
-						putchar(i);
-						if (i == 57 && e == 56 && a == 55)
-						{
-							break;
-						}
-						putchar(',');
-						putchar(' ')
+						break;
 					}
-					i++;
+					putchar(',');
+					putchar(' ');
 				}
-				e++;
+				i++;
 			}
-			a++;
+			e++;
 		}
-		putchar('/n');
-		return (0);
-}		
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
