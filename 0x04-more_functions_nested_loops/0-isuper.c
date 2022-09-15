6e88f19f6f882f2e5b85e8905976d7ad5964956e
MrMvshv <machariajosphat001@gmail.com>
2022-09-15 14:56:55 +0000

    0-isuper?

---
+++ 0x04-more_functions_nested_loops/0-isuper.c
@@ -0,0 +1,13 @@
+nclude "main.h"
+/**
+ *  * _isupper -  checks for uppercase
+ *   * @c: character to check (ascii int)
+ *    * Return: 1 for uppercase, 0 otherwise
+ *     */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
