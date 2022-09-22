6e022c7792a92a24f993363e9f90a6291abece46
MrMvshv <machariajosphat001@gmail.com>
2022-09-22 08:43:52 +0000

    2.2

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * string_toupper - lowercase to uppercase
+ * @*: pointer to string
+ *
+ * Return: pointer to uppercase string
+ */
+
+char *string_toupper(char *s)
+{
+	char *ptr = s;
+	char up;
+	int i = 0;
+
+	while (*ptr != 0)
+	{
+		if (*ptr > 96 && *ptr < 123)
+		{
+			up = *ptr - 32;
+			*ptr = up;
+		}
+		ptr++;
+		i++;
+	}
+	
+	ptr = ptr - i;
+
+	return (ptr);
+}
