98c357422902f4891e726a123efa06ab2ebc48d2
MrMvshv <machariajosphat001@gmail.com>
2022-09-22 09:59:14 +0000

    5.betty2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int checker(char *ptr);
+
 /**
  * cap_string - lowercase to uppercase
  * @s: pointer to string
@@ -12,7 +14,7 @@ char *cap_string(char *s)
 	char *ptr = s;
 	char up;
 	int i = 0;
-	int cd = 0;
+	int cd;
 
 	if (*ptr > 96 && *ptr < 123)
 	{
@@ -23,18 +25,7 @@ char *cap_string(char *s)
 
 	while (*ptr != 0)
 	{
-		if (*ptr == ' ' || *ptr == '	' || *ptr == ',')
-			cd = 1;
-		else if (*ptr == '\n' || *ptr == ';' || *ptr == '.')
-			cd = 1;
-		else if (*ptr == '!' || *ptr == '?' || *ptr == '\"')
-			cd = 1;
-		else if (*ptr == ')' || *ptr == '(' || *ptr == '{')
-			cd = 1;
-		else if (*ptr == '}')
-			cd = 1;
-		else
-			cd = 0;
+		cd = checker(ptr);
 
 		if (cd == 1)
 		{
@@ -60,4 +51,30 @@ char *cap_string(char *s)
 	ptr = ptr - i;
 
 	return (ptr);
+}
+/**
+ * checker - checks for breaks
+ * @ptr: string
+ *
+ * Return: 1 0r 0
+ */
+
+int checker(char *ptr)
+{
+	int cd;
+
+	if (*ptr == ' ' || *ptr == '\t' || *ptr == ',')
+		cd = 1;
+	else if (*ptr == '\n' || *ptr == ';' || *ptr == '.')
+		cd = 1;
+	else if (*ptr == '!' || *ptr == '?' || *ptr == '\"')
+		cd = 1;
+	else if (*ptr == ')' || *ptr == '(' || *ptr == '{')
+		cd = 1;
+	else if (*ptr == '}')
+		cd = 1;
+	else
+		cd = 0;
+
+	return (cd);
 }
