96f1877a55f1f9d4be5b129914c75ccf1ea39896
MrMvshv <machariajosphat001@gmail.com>
2022-09-28 08:17:46 +0000

    ReCurSe1v

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+int endA(char *s, int n);
+
+int n = 0;
+int m = 0;
+int flg = 0;
+
+/**
+ * _print_rev_recursion - prints the string's reverse
+ * @s: the string pointer
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (n == 0 && flg == 0)
+	{
+		n = endA(s, m);
+	}
+	n -= 1;
+
+	_putchar(s[n]);
+
+	if (n > 0)
+	{
+		_print_rev_recursion(s);
+		s--;
+	}
+	return;
+
+}
+/**
+ * endA - gets length and takes pointer to end
+ * @s: string pointer
+ * @n: length variable
+ *
+ * Return: length of string
+ */
+int endA(char *s, int n)
+{
+	flg = 1;
+
+	if (*s != '\0')
+	{
+		n += 1;
+		s++;
+		n = endA(s, n);
+	}
+
+	return (n);
+
+}
