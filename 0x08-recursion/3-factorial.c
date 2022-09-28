bf0bbd4c126d3a5d9d3c7673bd6f8f18d190f0fd
MrMvshv <machariajosphat001@gmail.com>
2022-09-28 12:55:30 +0000

    ReCurSe1v

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,9 +7,9 @@
  * Return: the factorial
  */
 
- int factorial(int n)
- {
- 	if (n < 0)
+int factorial(int n)
+{
+	if (n < 0)
 	{
 		return (-1);
 	}
@@ -18,5 +18,5 @@
 		return (1);
 	}
 
-	return (n * factorial(n-1));
- }
+	return (n * factorial(n - 1));
+}
