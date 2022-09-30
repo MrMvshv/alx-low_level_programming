ed7bd9df81a46827449d61bd5335ce5682d9be67
MrMvshv <machariajosphat001@gmail.com>
2022-09-30 06:50:18 +0000

    hey, my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the program's name
+ * @argc: number of CL arguments
+ * @argv: array of CL arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
