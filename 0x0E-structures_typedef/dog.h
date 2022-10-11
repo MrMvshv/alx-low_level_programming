41f9671c573e53a62ddc523e28afe68e24efcc1a
MrMvshv <machariajosphat001@gmail.com>
2022-10-11 13:38:26 +0000

    4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,12 +16,14 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 /**
  * dog_t - Typedef for dog struct
  */
 typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 
 #endif
