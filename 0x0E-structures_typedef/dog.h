0641f18dff7463307eabd6dbcf05a2370bfcaa9a
MrMvshv <machariajosphat001@gmail.com>
2022-10-11 12:33:30 +0000

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: the owner
+ *
+ * Dog is defined as follows
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
