460fe53011d97cab14a381c8a9725d56f531ba40
MrMvshv <machariajosphat001@gmail.com>
2022-10-24 13:48:49 +0000

    3

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+/**
+ * _strlen - returns length of string
+ * @s: the string pointer
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != 0)
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
+/**
+ * add_node- adds a node to beginning of a list
+ * @head: head pointer
+ * @str: string to duplicate
+ *
+ * Return: address of new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+		return (NULL);
+
+	new->len = _strlen((char *)str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+/**
+ * _strlen - returns length of string
+ * @s: the string pointer
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != 0)
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
+/**
+ * add_node_end- adds a node to end of a list
+ * @head: head pointer
+ * @str: string to duplicate
+ *
+ * Return: address of new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+		return (NULL);
+
+	new->len = _strlen((char *)str);
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		list_t *last = *head;
+
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+
+		last->next = new;
+	}
+	return (new);
+}
