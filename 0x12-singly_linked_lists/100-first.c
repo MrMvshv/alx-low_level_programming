dbb835a5afed76978a203ef77425b5fa504b8507
MrMvshv <machariajosphat001@gmail.com>
2022-10-24 14:26:28 +0000

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * beforeMain - prints before main
+ *
+ * Return: void
+ */
+void beforeMain(void)
+{
+	char *s;
+
+	s = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+	printf("%s", s);
+}
