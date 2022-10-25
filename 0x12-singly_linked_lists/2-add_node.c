ab3e3265d3e57ff269f43949986ac4add519ed39
MrMvshv <machariajosphat001@gmail.com>
2022-10-25 08:26:41 +0000

    second

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -29,11 +29,17 @@ list_t *add_node(list_t **head, const char *str)
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 
 	new->str = strdup(str);
 	if (new->str == NULL)
+	{
+		free(new->str);
 		return (NULL);
+	}
 
 	new->len = _strlen((char *)str);
 	new->next = *head;
