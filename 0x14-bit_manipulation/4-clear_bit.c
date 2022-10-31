e14ebce967bb8023e237c5bb7808811b53df9a64
MrMvshv <machariajosphat001@gmail.com>
2022-10-31 15:42:03 +0000

    4444

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,9 +1,15 @@
 #include "main.h"
-
+/**
+ * clear_bit - change bit to 0
+ * @n: number
+ * @index: index
+ *
+ * Return: 1 or 0
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
-			return (-1);
+		return (-1);
 
 	*n &= ~(1 << index);
 	return (1);
