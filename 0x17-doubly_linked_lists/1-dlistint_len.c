b78be2c8c57f333e9047616653849ad191cfc6e7
MrMvshv <machariajosphat001@gmail.com>
2022-12-08 18:49:00 +0000

    1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print all elements of a dlist
+ * @h: list pointer
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n = 0;
+	dlistint_t *current = NULL;
+
+	current = (dlistint_t *)h;
+	while (current)
+	{
+		n++;
+		current = current->next;
+	}
+	return (n);
+}
