4a679d74e177d122db5f309e3dacfe646775dee2
MrMvshv <machariajosphat001@gmail.com>
2023-01-07 17:54:22 +0000

    2hk

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element 2 #table
+ * @ht: # table
+ * @key: key
+ * @value: value to add
+ *
+ * Return: 1 on sucess, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx = 0;
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[idx] != NULL)
+	{
+		if (ht->array[0] != NULL)
+			return (0);
+		idx = 0;
+	}
+	ht->array[idx] = malloc(sizeof(hash_node_t));
+	if (ht->array[idx] == NULL)
+		return (0);
+	ht->array[idx]->key = strdup(key);
+	ht->array[idx]->value = strdup(value);
+	ht->array[idx]->next = NULL;
+	return (1);
+
+}
