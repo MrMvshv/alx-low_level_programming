05ce89af2d5c0111a4727ac774ed04ac79269bc5
MrMvshv <machariajosphat001@gmail.com>
2023-01-08 14:30:23 +0000

    4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves value associated with key
+ * @ht: hash table
+ * @key: key
+ *
+ * Return: value or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx = 0;
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[idx] == NULL)
+		return (NULL);
+	return (ht->array[idx]->value);
+}
