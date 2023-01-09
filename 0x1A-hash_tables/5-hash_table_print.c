dd111310d0c354cd6ba333c9600324e588c43bf8
MrMvshv <machariajosphat001@gmail.com>
2023-01-09 06:24:21 +0000

    55

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,5 +1,23 @@
 #include "hash_tables.h"
-
+/**
+ * test_hash - checks whether value printed is last
+ * @ht: hash table
+ * @idx: index
+ *
+ * Return: 1 if not last
+ */
+int test_hash(const hash_table_t *ht, unsigned long int idx)
+{
+	unsigned long int s = 0;
+	for (s = (idx + 1); s < ht->size; s++)
+	{
+		if (ht->array[s] == NULL)
+			continue;
+		else
+			return (1);
+	}
+	return (0);
+}
 /**
  * hash_table_print - prints hash table
  * @ht: #table 2 print
@@ -17,7 +35,10 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		if (ht->array[s] == NULL)
 			continue;
-		printf("\'%s\':\'%s\', ", ht->array[s]->key, ht->array[s]->value);
+		printf("\'%s\':\'%s\'", ht->array[s]->key, ht->array[s]->value);
+		if (test_hash(ht, s) == 1)
+			printf(", ");
+
 	}
 	printf("}\n");
 }
