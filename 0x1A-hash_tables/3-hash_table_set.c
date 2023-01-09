63d8d1496fcab14fbf2793af443e2d475183bebd
MrMvshv <machariajosphat001@gmail.com>
2023-01-09 05:48:31 +0000

    33

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,27 +10,43 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int idx = 0;
+	unsigned long int idx = 0, i;
+	hash_node_t *nw;
+	char *val;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	val = strdup(value);
+	if (val == NULL)
+		return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[idx] != NULL)
-	{
-		if (ht->array[0] != NULL)
-			return (0);
 		idx = 0;
+
+	for (i = idx; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = val;
+			return (1);
+		}
 	}
-	ht->array[idx] = malloc(sizeof(hash_node_t));
-	if (ht->array[idx] == NULL)
-	{	free(ht->array[idx]);
+	nw = malloc(sizeof(hash_node_t));
+	if (nw == NULL)
+	{	free(val);
 		return (0);
 	}
-	ht->array[idx]->key = strdup(key);
-	if (ht->array[idx]->key == NULL)
-	{	free(ht->array[idx]->key);
-		free(ht->array[idx]);
+	nw->key = strdup(key);
+	if (nw->key == NULL)
+	{	free(val);
+		free(nw);
 		return (0);
 	}
-	ht->array[idx]->value = strdup(value);
-	ht->array[idx]->next = NULL;
+	nw->value = val;
+	nw->next = ht->array[idx];
+	ht->array[idx] = nw;
 	return (1);
 }
