eac180c66c84077195cc3d2c1e729024672acc4f
MrMvshv <machariajosphat001@gmail.com>
2023-02-26 13:13:02 +0300

    interland

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+returns perimeter of island described in grid
+grid is a list of list of integers
+0 -  water zone;1 - land zone
+1 cell = square, side length 1
+Grid cells are not diagonally connected
+Grid is rectangular, width and height don’t exceed 100
+Grid is completely surrounded by water, and there is one island (or nothing).
+The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
+"""
+def island_perimeter(grid):
+    """ calculates perimeter of island
+        represemted in grid
+    """
+    perimeter = 0
+    x, y = 0, 0
+    height = len(grid)
+    width = len(grid)
+
+    while y < height:
+        x = 0
+        while x < width:
+            cell = grid[y][x]
+            print(f"coordinates: {x},{y}")
+            print("cell is :", cell)
+            if cell != 0:
+                if y == 0 or grid[y - 1][x] == 0:
+                    perimeter += 1
+                if grid[y][x - 1] == 0:
+                    perimeter += 1
+                if grid[y][x + 1] == 0:
+                    perimeter += 1
+                if grid[y + 1][x] == 0:
+                    perimeter += 1
+            x += 1
+        y += 1
+    return perimeter
